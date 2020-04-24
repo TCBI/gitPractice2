955cfda9e4690e2e69301be63497982668744fa0
TCBI <cibairhtc@gmail.com>
2020-04-24 16:14:43 -0400

    Change in helloworld2

--- src/day_1/helloworld2.java
+++ src/day_1/helloworld2.java
@@ -1,8 +1,11 @@
 package day_1;
 
+
 public class helloworld2 {
     public static void main(String[] args) {
         System.out.println("hello world 2");
+
+        System.out.println("Some change");
     }
 
 }
